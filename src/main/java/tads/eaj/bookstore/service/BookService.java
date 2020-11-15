1433a79c5054c4a5de10cef51de3161e3d72e15d
danielbras <carloosdd9550@gmail.com>
2020-11-15 15:55:35 -0300

    added the bean service

---
+++ src/main/java/tads/eaj/bookstore/service/BookService.java
@@ -0,0 +1,35 @@
+package tads.eaj.bookstore.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tads.eaj.bookstore.model.Book;
+import tads.eaj.bookstore.reporitory.BookRepository;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    private BookRepository repository;
+
+    @Autowired
+    public void setRepository(BookRepository repository){
+        this.repository = repository;
+    }
+
+    public List<Book> findAll(){
+        return repository.findAll();
+    }
+
+    public void save(Book b){
+        repository.save(b);
+    }
+
+    public void delete(Long id){
+        repository.deleteById(id);
+    }
+
+    public Book getById(Long id){
+    return repository.getOne(id);
+    }
+}
