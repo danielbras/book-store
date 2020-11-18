65e34cf08397a94da748e4a28ea6c7320e491d6a
danielbras <carloosdd9550@gmail.com>
2020-11-17 23:13:26 -0300

    added new features in the system routes

--- src/main/java/tads/eaj/bookstore/controller/BookController.java
+++ src/main/java/tads/eaj/bookstore/controller/BookController.java
@@ -4,12 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import tads.eaj.bookstore.model.Book;
 import tads.eaj.bookstore.service.BookService;
-
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.util.Calendar;
 import java.util.List;
 
 @Controller
@@ -23,9 +26,15 @@ public class BookController {
     }
 
     @RequestMapping("/")
-    public String getHome(Model model){
+    public String getHome(@ModelAttribute(value = "feedback") Book book, Model model, HttpServletResponse response ){
+        Calendar calendar = Calendar.getInstance();
+        String access = calendar.getTime().toString().replace(" ", "_");
+        Cookie c = new Cookie("lastAccess", access);
+        response.addCookie(c);
+
         List<Book> book_list = service.findAll();
         model.addAttribute("book_list", book_list);
+        model.addAttribute("feedbackMessage", book);
         return "home";
     }
 
@@ -44,9 +53,15 @@ public class BookController {
     }
 
     @RequestMapping(value = "/save", method = RequestMethod.POST)
-    public String doSave(@ModelAttribute Book book, Model model){
-        service.save(book);
-        return "redirect:/";
+    @GetMapping("/save")
+    public String doSave(@ModelAttribute (name = "new_book") @Valid Book book, Errors errors, RedirectAttributes redirectAttributes){
+        if(errors.hasErrors()){
+            return "register";
+        } else {
+            service.save(book);
+            redirectAttributes.addFlashAttribute("feedback", book);
+            return "redirect:/";
+        }
     }
 
     @RequestMapping("/edit/{id}")
