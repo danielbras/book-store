ebf3be33487d5a51d52e377192e00636bc499ff8
danielbras <carloosdd9550@gmail.com>
2020-11-17 23:12:39 -0300

    added validation for the database tables

--- src/main/java/tads/eaj/bookstore/model/Book.java
+++ src/main/java/tads/eaj/bookstore/model/Book.java
@@ -3,8 +3,13 @@ package tads.eaj.bookstore.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.*;
 import java.sql.Date;
 
 @AllArgsConstructor
@@ -12,14 +17,30 @@ import java.sql.Date;
 @Data
 @Entity
 public class Book {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     long id;
+
+    @Size(min = 5, max = 60, message = "O título deve ter entre 5 e 60 caracteres!")
+    @NotBlank(message = "O campo não deve estar em branco")
     String title;
-    String subtitle;
+
+    @Size(min = 3, max = 60, message = "O nome do autor deve ter entre 3 e 60 caracteres!")
+    @NotBlank(message = "O campo não deve estar em branco")
     String author;
-    String description;
-    String language;
+
+    @DecimalMin(value = "0", message = "Deve ser maior ou igual a zero")
+    @NotNull(message = "O campo não deve estar em branco")
     Double price;
+
+    @NotBlank(message = "O campo não deve estar em branco")
+    String subtitle;
+
     Date release_date;
+
+    String description;
+
+    String language;
+
 }
